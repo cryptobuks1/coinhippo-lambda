4eb7791fd7b388dd855f148c88917372e8173c92
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-08-30 23:14:39 +0700

    caching

--- requester/index.js
+++ requester/index.js
@@ -5,7 +5,10 @@
 exports.handler = async (event, context, callback) => {
   // import module for submitting request.
   const axios = require('axios');
+
+  // import modules
   const { parachains } = require('./data');
+  const moment = require('moment');
 
   /************************************************
    * External API information for requesting data
@@ -42,6 +45,10 @@ exports.handler = async (event, context, callback) => {
       api_host: process.env.SUBSCAN_API_HOST || 'https://{chain}.api.subscan.io/api/',
       api_key: process.env.SUBSCAN_API_KEY || '{YOUR_SUBSCAN_API_KEY}',
     },
+    cache: {
+      api_host: process.env.DYNAMODB_API_HOST || '{YOUR_DYNAMODB_API_HOST}',
+      table_name: process.env.DYNAMODB_CACHE_TABLE_NAME || '{YOUR_DYNAMODB_CACHE_TABLE_NAME}',
+    },
     feeds: {
       api_host: process.env.DYNAMODB_API_HOST || '{YOUR_DYNAMODB_API_HOST}',
       table_name: process.env.DYNAMODB_FEEDS_TABLE_NAME || '{YOUR_DYNAMODB_FEEDS_TABLE_NAME}',
@@ -75,6 +82,7 @@ exports.handler = async (event, context, callback) => {
 
     // initial response object
     let res = null;
+    let resCache = null;
 
     // initial path parameter
     let path = event.queryStringParameters.path;
@@ -83,8 +91,32 @@ exports.handler = async (event, context, callback) => {
       delete event.queryStringParameters.path;
     }
 
+    // initial cacher object
+    const cacher = axios.create({ baseURL: env.cache.api_host });
+
+    // generate url string
+    const generateUrl = (url, params, paramsFilterOut) => {
+      url = url || '/';
+
+      return [url, Object.entries({ ...params }).filter(([param, value]) => !(paramsFilterOut && paramsFilterOut.includes(param))).map(entry => entry.join('=')).join('&')].filter(urlPart => urlPart).join('?');
+    };
+
+    // get cache
+    const getCache = async id => await cacher.get('', { params: { table_name: env.cache.table_name, method: 'get', ID: id } })
+      .catch(error => { return { data: null }; });
+
+    // set cache
+    const setCache = async data => await cacher.post('', { table_name: env.cache.table_name, method: 'put', ...data })
+      .catch(error => { return { data: null }; });
+
     // initial params parameter
     let params = null;
+
+    // declare id
+    let id;
+
+    // initial current time
+    const time = moment();
 
     // seperate each api
     switch (apiName) {
@@ -94,10 +126,27 @@ exports.handler = async (event, context, callback) => {
         // setup query string parameters including limit
         params = { limit: 31, ...event.queryStringParameters };
 
-        // send request
-        res = await requester.get(path, { params })
-          // set response data from error handled by exception
-          .catch(error => { return { data: { data: null, metadata: { error } } }; });
+        // generate id
+        id = `${apiName}_${generateUrl(path, params)}`;
+
+        // get cache
+        resCache = await getCache(id);
+
+        // check cache
+        if (resCache && resCache.data && resCache.data && resCache.data.data.Json && resCache.data.data.Expired > time.valueOf()) {
+          res = { data: JSON.parse(resCache.data.data.Json) };
+        }
+        else {
+          // send request
+          res = await requester.get(path, { params })
+            // set response data from error handled by exception
+            .catch(error => { return { data: { data: null, metadata: { error } } }; });
+
+          if (res && res.data && res.data.data) {
+            // set cache
+            await setCache({ ID: id, API: apiName, Expired: moment(time).add(12, 'hour').valueOf(), Json: JSON.stringify(res.data) });
+          }
+        }
         break;
       case 'news':
         // normalize path parameter
@@ -117,10 +166,27 @@ exports.handler = async (event, context, callback) => {
         // setup query string parameters including API key
         params = { apikey: env[apiName].api_key, ...event.queryStringParameters };
 
-        // send request
-        res = await requester.get(path, { params })
-          // set response data from error handled by exception
-          .catch(error => { return { data: { result: null, status: 0, message: error.message } }; });
+        // generate id
+        id = `${apiName}_${generateUrl(path, params, ['apikey'])}`;
+
+        // get cache
+        resCache = await getCache(id);
+
+        // check cache
+        if (resCache && resCache.data && resCache.data && resCache.data.data.Json && resCache.data.data.Expired > time.valueOf()) {
+          res = { data: JSON.parse(resCache.data.data.Json) };
+        }
+        else {
+          // send request
+          res = await requester.get(path, { params })
+            // set response data from error handled by exception
+            .catch(error => { return { data: { result: null, status: 0, message: error.message } }; });
+
+          if (res && res.data && res.data.result) {
+            // set cache
+            await setCache({ ID: id, API: apiName, Expired: moment(time).add(20, 'second').valueOf(), Json: JSON.stringify(res.data) });
+          }
+        }
         break;
       case 'coingecko':
         // normalize path parameter
@@ -128,10 +194,57 @@ exports.handler = async (event, context, callback) => {
         // setup query string parameters including limit
         params = { ...event.queryStringParameters };
 
-        // send request
-        res = await requester.get(path, { params })
-          // set response data from error handled by exception
-          .catch(error => { return { data: { error } }; });
+        // generate id
+        id = `${apiName}_${generateUrl(path, params)}`;
+
+        // get cache
+        resCache = await getCache(id);
+
+        // check cache
+        if (resCache && resCache.data && resCache.data && resCache.data.data.Json && resCache.data.data.Expired > time.valueOf()) {
+          res = { data: JSON.parse(resCache.data.data.Json) };
+        }
+        else {
+          // send request
+          res = await requester.get(path, { params })
+            // set response data from error handled by exception
+            .catch(error => { return { data: { error } }; });
+
+          if (res && res.data && !res.data.error) {
+            let expired;
+
+            switch (path) {
+              case '/search':
+              case '/coins/categories/list':
+                expired = moment(time).add(1, 'day').valueOf();
+                break;
+              case '/exchange_rates':
+              case '/global':
+              case '/global/decentralized_finance_defi':
+              case '/simple/price':
+              case '/coins/markets':
+              case '/coins/categories':
+              case '/derivatives':
+              case '/exchanges':
+              case '/derivatives/exchanges':
+                expired = moment(time).add(1, 'minute').valueOf();
+                break;
+              case '/search/trending':
+                expired = moment(time).add(5, 'minute').valueOf();
+                break;
+              default:
+                if (path.startsWith('/companies/public_treasury/')) {
+                  expired = moment(time).add(1, 'hour').valueOf();
+                }
+                else {
+                  expired = moment(time).add(1, 'minute').valueOf();
+                }
+            }
+
+            // set cache
+            await setCache({ ID: id, API: apiName, Expired: expired, Json: JSON.stringify(res.data) });
+          }
+        }
         break;
       case 'covalent':
         // normalize path parameter
@@ -140,10 +253,27 @@ exports.handler = async (event, context, callback) => {
         // setup query string parameters including API key
         params = { key: env[apiName].api_key, ...event.queryStringParameters };
 
-        // send request
-        res = await requester.get(path, { params })
-          // set response data from error handled by exception
-          .catch(error => { return { data: { data: null, error: true, error_message: error.message, error_code: error.code } }; });
+        // generate id
+        id = `${apiName}_${generateUrl(path, params, ['key'])}`;
+
+        // get cache
+        resCache = await getCache(id);
+
+        // check cache
+        if (resCache && resCache.data && resCache.data && resCache.data.data.Json && resCache.data.data.Expired > time.valueOf()) {
+          res = { data: JSON.parse(resCache.data.data.Json) };
+        }
+        else {
+          // send request
+          res = await requester.get(path, { params })
+            // set response data from error handled by exception
+            .catch(error => { return { data: { data: null, error: true, error_message: error.message, error_code: error.code } }; });
+
+          if (res && res.data && res.data.data) {
+            // set cache
+            await setCache({ ID: id, API: apiName, Expired: moment(time).add(1, 'minute').valueOf(), Json: JSON.stringify(res.data) });
+          }
+        }
         break;
       case 'whale_alert':
         // normalize path parameter
@@ -173,19 +303,36 @@ exports.handler = async (event, context, callback) => {
         // setup query string parameters including limit
         params = Object.fromEntries(new Map(Object.entries(event.queryStringParameters).map(([key, value]) => [key, !isNaN(value) ? Number(value) : (value && value.includes(',')) || ['status'].includes(key) ? value.split(',') : value])));
 
-        // send request
-        res = await requester.post(path, { ...params }, { headers: { 'X-API-Key': env[apiName].api_key } })
-          // set response data from error handled by exception
-          .catch(error => { return { data: { data: null, message: error.message, code: error.code } }; });
+        // generate id
+        id = `${apiName}_${generateUrl(path, params)}`;
 
-        // custom project data
-        if (path === '/scan/parachain/list' && res && res.data && res.data.data && res.data.data.chains) {
-          res.data.data.chains = res.data.data.chains.map(projectData => {
-            const paraData = (parachains[chain] && parachains[chain][projectData.para_id]) || {};
-            const { name, image, symbol } = { ...paraData };
+        // get cache
+        resCache = await getCache(id);
 
-            return { ...projectData, name, image, symbol, paraData };
-          });
+        // check cache
+        if (resCache && resCache.data && resCache.data && resCache.data.data.Json && resCache.data.data.Expired > time.valueOf()) {
+          res = { data: JSON.parse(resCache.data.data.Json) };
+        }
+        else {
+          // send request
+          res = await requester.post(path, { ...params }, { headers: { 'X-API-Key': env[apiName].api_key } })
+            // set response data from error handled by exception
+            .catch(error => { return { data: { data: null, message: error.message, code: error.code } }; });
+
+          // custom project data
+          if (path === '/scan/parachain/list' && res && res.data && res.data.data && res.data.data.chains) {
+            res.data.data.chains = res.data.data.chains.map(projectData => {
+              const paraData = (parachains[chain] && parachains[chain][projectData.para_id]) || {};
+              const { name, image, symbol } = { ...paraData };
+
+              return { ...projectData, name, image, symbol, paraData };
+            });
+          }
+
+          if (res && res.data && res.data.data) {
+            // set cache
+            await setCache({ ID: id, API: apiName, Expired: moment(time).add(5, 'minute').valueOf(), Json: JSON.stringify(res.data) });
+          }
         }
         break;
       case 'feeds':
